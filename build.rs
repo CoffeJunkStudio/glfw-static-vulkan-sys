d2b118ab665df728b19b86b6d87ebef6a12058f1
Dezajno <dezajno@coffe.junk.studio>
2021-04-18 16:32:14 +0200

    Enable GLFW_VULKAN_STATIC option only when targeting macos or ios

--- build.rs
+++ build.rs
@@ -2,16 +2,26 @@ extern crate cmake;
 use cmake::Config;
 
 fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    let vulkan_static = {
+        if target_os == "macos" || target_os == "ios" {
+            "ON"
+        } else {
+            "OFF"
+        }
+    };
+
     let dst = Config::new(".")
         .define("GLFW_BUILD_EXAMPLES", "OFF")
         .define("GLFW_BUILD_TESTS", "OFF")
         .define("GLFW_BUILD_DOCS", "OFF")
-        .define("GLFW_VULKAN_STATIC", "ON")
+        .define("GLFW_VULKAN_STATIC", vulkan_static)
         .define("CMAKE_INSTALL_LIBDIR", "lib")
         .build();
     
     let lib_dir = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("lib");
-    std::os::unix::fs::symlink(lib_dir.join("libglfw3.a"), lib_dir.join("libglfw.a"));
+    // Ignore existing symlink error
+    let _ = std::os::unix::fs::symlink(lib_dir.join("libglfw3.a"), lib_dir.join("libglfw.a"));
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
-    println!("cargo:rustc-link-lib=dylib=glfw3");
 }
